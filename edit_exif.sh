673ff7b3422ff42defd5710574c5e88eafd991f4
kunihiko harada <sthsoulful@gmail.com>
2017-06-26 22:35:41 +0900

    first commit

---
+++ edit_exif.sh
@@ -0,0 +1,23 @@
+
+# exiftool -gpslatituderef=n -gpslongituderef=e -gpslatitude=34.381523 -gpslongitude=133.012533 -overwrite_original *.JPG # 運動公園
+# exiftool -gpslatituderef=n -gpslongituderef=e -gpslatitude=35.580602 -gpslongitude=139.561983 -overwrite_original *.JPG # たまプラじたく
+exiftool -gpslatituderef=n -gpslongituderef=e -gpslatitude=35.570025 -gpslongitude=139.535572 -overwrite_original *.JPG # あざみ野しらゆり
+# exiftool -gpslatituderef=n -gpslongituderef=e -gpslatitude=34.403012 -gpslongitude=133.192319 -overwrite_original *.JPG # 尾道駅U2
+# exiftool -gpslatituderef=n -gpslongituderef=e -gpslatitude=34.410474 -gpslongitude=133.198709 -overwrite_original *.JPG # 千光寺
+# exiftool -gpslatituderef=n -gpslongituderef=e -gpslatitude=34.389360 -gpslongitude=133.012683 -overwrite_original *.JPG # 三原実家
+# exiftool -gpslatituderef=n -gpslongituderef=e -gpslatitude=34.565222 -gpslongitude=133.678658 -overwrite_original shinkurashiki/*.JPG # 新倉敷
+# exiftool -gpslatituderef=n -gpslongituderef=e -gpslatitude=34.714759 -gpslongitude=135.527490 -overwrite_original oosaka/*.JPG #yui実家 
+# exiftool -gpslatituderef=n -gpslongituderef=e -gpslatitude=34.761888 -gpslongitude=135.617719 -overwrite_original neyagawa/*.JPG #neyagawa
+# exiftool -gpslatituderef=n -gpslongituderef=e -gpslatitude=34.695977 -gpslongitude=135.512465 -overwrite_original hastsumode/*.JPG #大阪天満宮
+# exiftool -gpslatituderef=n -gpslongituderef=e -gpslatitude=35.560316 -gpslongitude=139.487119 -overwrite_original *.JPG #こどもの国
+# exiftool -gpslatituderef=n -gpslongituderef=e -gpslatitude=35.7436834 -gpslongitude=139.6430999 -overwrite_original *.JPG #豊島園
+# exiftool -gpslatituderef=n -gpslongituderef=e -gpslatitude=35.6030613 -gpslongitude=139.1989093 -overwrite_original *.JPG #相模湖プレジャーフォレスト
+# exiftool -gpslatituderef=n -gpslongituderef=e -gpslatitude=35.578093 -gpslongitude=139.560537 -overwrite_original tamapuraparking/*.JPG #美しが丘保育園隣の駐車場
+# exiftool -gpslatituderef=n -gpslongituderef=e -gpslatitude=34.305662 -gpslongitude=132.995451 -overwrite_original *.JPG #大久野島
+# exiftool -gpslatituderef=n -gpslongituderef=e -gpslatitude=34.532013 -gpslongitude=132.775717 -overwrite_original kohan/*.JPG #湖畔の里福富
+
+
+
+exiftool -alldates='2017:05:25 12:00:00' -overwrite_original *.JPG
+
+# exiftool -AllDates+='10:11:26 0:0:0' *.JPG
