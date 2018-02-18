9c65a74e7c929dd0f1862a5d3ad98b10a52313da
haradalibtech <sthsoulful@gmail.com>
2018-02-18 21:09:34 +0900

    modified

--- edit_exif.sh
+++ edit_exif.sh
@@ -1,12 +1,21 @@
-
 # exiftool -gpslatituderef=n -gpslongituderef=e -gpslatitude=34.381523 -gpslongitude=133.012533 -overwrite_original *.JPG # 運動公園
+# exiftool -gpslatituderef=n -gpslongituderef=e -gpslatitude=35.624710 -gpslongitude=139.517588 -overwrite_original *.JPG # よみうりランド
+# exiftool -gpslatituderef=n -gpslongituderef=e -gpslatitude=35.581343 -gpslongitude=139.516677 -overwrite_original *.JPG # 琴平神社
+# exiftool -gpslatituderef=n -gpslongituderef=e -gpslatitude=35.578273 -gpslongitude=139.554212 -overwrite_original *.jpg # ラカンスタジオ
+# exiftool -gpslatituderef=n -gpslongituderef=e -gpslatitude=35.575100 -gpslongitude=139.554688 -overwrite_original *.JPG # 五味家
+# exiftool -gpslatituderef=n -gpslongituderef=e -gpslatitude=35.568021 -gpslongitude=139.532971 -overwrite_original *.JPG # 黒須田小学校
 # exiftool -gpslatituderef=n -gpslongituderef=e -gpslatitude=35.580602 -gpslongitude=139.561983 -overwrite_original *.JPG # たまプラじたく
+# exiftool -gpslatituderef=n -gpslongituderef=e -gpslatitude=35.581360 -gpslongitude=139.516692 -overwrite_original *.jpg # 琴平神社
+# exiftool -gpslatituderef=n -gpslongituderef=e -gpslatitude=35.609814 -gpslongitude=139.573592 -overwrite_original *.JPG # 藤子不二雄ミュージアム
 exiftool -gpslatituderef=n -gpslongituderef=e -gpslatitude=35.570025 -gpslongitude=139.535572 -overwrite_original *.JPG # あざみ野しらゆり
 # exiftool -gpslatituderef=n -gpslongituderef=e -gpslatitude=34.403012 -gpslongitude=133.192319 -overwrite_original *.JPG # 尾道駅U2
 # exiftool -gpslatituderef=n -gpslongituderef=e -gpslatitude=34.410474 -gpslongitude=133.198709 -overwrite_original *.JPG # 千光寺
 # exiftool -gpslatituderef=n -gpslongituderef=e -gpslatitude=34.389360 -gpslongitude=133.012683 -overwrite_original *.JPG # 三原実家
+# exiftool -gpslatituderef=n -gpslongituderef=e -gpslatitude=34.353011 -gpslongitude=133.081939 -overwrite_original *.JPG # 須波海浜 
+# exiftool -gpslatituderef=n -gpslongituderef=e -gpslatitude=34.440441 -gpslongitude=132.919016 -overwrite_original *.JPG #  広島空港
+# exiftool -gpslatituderef=n -gpslongituderef=e -gpslatitude=34.487274 -gpslongitude=133.363914 -overwrite_original *.JPG # 福山
 # exiftool -gpslatituderef=n -gpslongituderef=e -gpslatitude=34.565222 -gpslongitude=133.678658 -overwrite_original shinkurashiki/*.JPG # 新倉敷
-# exiftool -gpslatituderef=n -gpslongituderef=e -gpslatitude=34.714759 -gpslongitude=135.527490 -overwrite_original oosaka/*.JPG #yui実家 
+# exiftool -gpslatituderef=n -gpslongituderef=e -gpslatitude=34.714759 -gpslongitude=135.527490 -overwrite_original *.JPG #yui実家 
 # exiftool -gpslatituderef=n -gpslongituderef=e -gpslatitude=34.761888 -gpslongitude=135.617719 -overwrite_original neyagawa/*.JPG #neyagawa
 # exiftool -gpslatituderef=n -gpslongituderef=e -gpslatitude=34.695977 -gpslongitude=135.512465 -overwrite_original hastsumode/*.JPG #大阪天満宮
 # exiftool -gpslatituderef=n -gpslongituderef=e -gpslatitude=35.560316 -gpslongitude=139.487119 -overwrite_original *.JPG #こどもの国
@@ -15,9 +24,28 @@ exiftool -gpslatituderef=n -gpslongituderef=e -gpslatitude=35.570025 -gpslongitu
 # exiftool -gpslatituderef=n -gpslongituderef=e -gpslatitude=35.578093 -gpslongitude=139.560537 -overwrite_original tamapuraparking/*.JPG #美しが丘保育園隣の駐車場
 # exiftool -gpslatituderef=n -gpslongituderef=e -gpslatitude=34.305662 -gpslongitude=132.995451 -overwrite_original *.JPG #大久野島
 # exiftool -gpslatituderef=n -gpslongituderef=e -gpslatitude=34.532013 -gpslongitude=132.775717 -overwrite_original kohan/*.JPG #湖畔の里福富
+# exiftool -gpslatituderef=n -gpslongituderef=e -gpslatitude=35.633090 -gpslongitude=139.880392 -overwrite_original *.JPG # ディズニーランド
+# exiftool -gpslatituderef=n -gpslongituderef=e -gpslatitude=34.704793 -gpslongitude=135.510436 -overwrite_original *.JPG # kidsplaza 
+# exiftool -gpslatituderef=n -gpslongituderef=e -gpslatitude=35.010664 -gpslongitude=135.758922 -overwrite_original *.JPG # 烏丸御池 
+# exiftool -gpslatituderef=n -gpslongituderef=e -gpslatitude=35.003827 -gpslongitude=135.759995 -overwrite_original *.JPG # 四条烏丸 
+# exiftool -gpslatituderef=n -gpslongituderef=e -gpslatitude=35.561309 -gpslongitude=139.549721 -overwrite_original *.JPG # 赤田西公園 サッカー
+# exiftool -gpslatituderef=n -gpslongituderef=e -gpslatitude=35.551126 -gpslongitude=139.779608 -overwrite_original *.JPG # 羽田
 
+# exiftool -gpslatituderef=n -gpslongituderef=e -gpslatitude=1.286350 -gpslongitude=103.859571 -overwrite_original *.JPG # art science
+# exiftool -gpslatituderef=n -gpslongituderef=e -gpslatitude=1.279933 -gpslongitude=103.863322 -overwrite_original *.JPG # garden by the bay
+# exiftool -gpslatituderef=n -gpslongituderef=e -gpslatitude=1.283411 -gpslongitude=103.859127 -overwrite_original *.JPG # marina bay sandas
+# exiftool -gpslatituderef=n -gpslongituderef=e -gpslatitude=1.301420 -gpslongitude=103.840235 -overwrite_original *.JPG # centerpoint
+# exiftool -gpslatituderef=n -gpslongituderef=e -gpslatitude=1.285614 -gpslongitude=103.852079 -overwrite_original *.JPG # boat quay
+# exiftool -gpslatituderef=n -gpslongituderef=e -gpslatitude=1.280334 -gpslongitude=103.850597 -overwrite_original *.JPG # lau pa sat
+# exiftool -gpslatituderef=n -gpslongituderef=e -gpslatitude=1.258017 -gpslongitude=103.820540 -overwrite_original *.JPG # sentosa sea aquarium
+# exiftool -gpslatituderef=n -gpslongituderef=e -gpslatitude=1.253842 -gpslongitude=103.821943 -overwrite_original *.JPG # sentosa uss
+# exiftool -gpslatituderef=n -gpslongituderef=e -gpslatitude=1.304851 -gpslongitude=103.837325 -overwrite_original *.JPG # nakanishi
+# exiftool -gpslatituderef=n -gpslongituderef=e -gpslatitude=1.293184 -gpslongitude=103.857119 -overwrite_original *.JPG # cool de sac
+# exiftool -gpslatituderef=n -gpslongituderef=e -gpslatitude=1.300694 -gpslongitude=103.838481 -overwrite_original *.JPG # 313 henmen
+# exiftool -gpslatituderef=n -gpslongituderef=e -gpslatitude=1.338264 -gpslongitude=103.693484 -overwrite_original *.JPG # pool
+# exiftool -gpslatituderef=n -gpslongituderef=e -gpslatitude=1.303918 -gpslongitude=103.810390 -overwrite_original *.JPG # dempsy hills
+# exiftool -gpslatituderef=n -gpslongituderef=e -gpslatitude=1.304716 -gpslongitude=103.839545 -overwrite_original *.JPG # tea
+# exiftool -gpslatituderef=n -gpslongituderef=e -gpslatitude=1.283866 -gpslongitude=103.843473 -overwrite_original *.JPG # chinatown
 
-
-exiftool -alldates='2017:05:25 12:00:00' -overwrite_original *.JPG
-
+exiftool -alldates='2017:12:09 12:00:00' -overwrite_original *.JPG
 # exiftool -AllDates+='10:11:26 0:0:0' *.JPG
